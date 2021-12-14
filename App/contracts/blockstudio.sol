9d3f0547e7ec7d7b5cf405427c597e544acc3dd8
Adwit <adwit18276@iiitd.ac.in>
2021-12-14 17:26:46 +0530

    added donateArtist in smart contract

--- App/contracts/blockstudio.sol
+++ App/contracts/blockstudio.sol
@@ -1,104 +1,136 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract blockstudio {
-    uint audienceIDTracker;
-    uint artistIDTracker;
-    uint songIDTracker;
-    
-    enum UserType {UNDEFINED, ARTIST, AUDIENCE}
-    
+    uint256 audienceIDTracker;
+    uint256 artistIDTracker;
+    uint256 songIDTracker;
+
+    enum UserType {
+        UNDEFINED,
+        ARTIST,
+        AUDIENCE
+    }
+
     struct Artist {
         string name;
-        uint artistID;
-        uint rating;
-        uint[] songsPublished;
+        uint256 artistID;
+        uint256[] songsPublished;
     }
-    
+
     struct Audience {
         string name;
-        uint audienceID;
-        uint[] songsPurchased;
-        mapping(uint => bool) isSongPurchased;
+        uint256 audienceID;
+        uint256[] songsPurchased;
+        mapping(uint256 => bool) isSongPurchased;
     }
-    
+
     struct Song {
         string songName;
         string artistName;
         string genre;
         string hash;
-        uint songID;
-        uint price;
+        uint256 songID;
+        uint256 price;
         address payable artistAddress;
     }
-    
-    mapping (address => UserType) identifyUser;
-    mapping (address => Artist) allArtists;
-    mapping (address => Audience) allAudience;
-    mapping (uint => Song) allSongs;
-    mapping (uint => uint) timesSongPurchased;
-    mapping (string => bool) songHashUsed;
-    
+
+    mapping(address => UserType) identifyUser;
+    mapping(address => Artist) allArtists;
+    mapping(address => Audience) allAudience;
+    mapping(uint256 => Song) allSongs;
+    mapping(uint256 => uint256) timesSongPurchased;
+    mapping(string => bool) songHashUsed;
+    mapping(uint256 => address payable) getArtistAddress;
+
     constructor() {
         audienceIDTracker = 0;
         artistIDTracker = 0;
         songIDTracker = 0;
     }
-    
-    function getNumSongs() public view returns(uint) {
+
+    function getNumSongs() public view returns (uint256) {
         return songIDTracker;
     }
 
-    function checkUser() public view returns(UserType) {
+    function checkUser() public view returns (UserType) {
         return identifyUser[msg.sender];
     }
-    
+
     function addNewArtist(string memory _name) public {
         artistIDTracker += 1;
-        
+
         Artist memory newArtist;
         newArtist.name = _name;
         newArtist.artistID = artistIDTracker;
-        newArtist.rating = 0;
-        
+        getArtistAddress[artistIDTracker] = payable(msg.sender);
+
         allArtists[msg.sender] = newArtist;
         identifyUser[msg.sender] = UserType.ARTIST;
     }
-    
+
     function addNewAudience(string memory _name) public {
         audienceIDTracker += 1;
-        
+
         Audience storage newAudience = allAudience[msg.sender];
         newAudience.name = _name;
         newAudience.audienceID = audienceIDTracker;
 
         identifyUser[msg.sender] = UserType.AUDIENCE;
     }
-    
-    function getAudienceDetails() public view returns(string memory, uint, uint[] memory){
-        return (allAudience[msg.sender].name, allAudience[msg.sender].audienceID, allAudience[msg.sender].songsPurchased);
+
+    function getAudienceDetails()
+        public
+        view
+        returns (
+            string memory,
+            uint256,
+            uint256[] memory
+        )
+    {
+        return (
+            allAudience[msg.sender].name,
+            allAudience[msg.sender].audienceID,
+            allAudience[msg.sender].songsPurchased
+        );
     }
-    
-    function getArtistDetails() public view returns(string memory, uint, uint, uint[] memory){
-        return (allArtists[msg.sender].name, 
-            allArtists[msg.sender].artistID, 
-            allArtists[msg.sender].rating, 
+
+    function getArtistDetails()
+        public
+        view
+        returns (
+            string memory,
+            uint256,
+            uint256[] memory
+        )
+    {
+        return (
+            allArtists[msg.sender].name,
+            allArtists[msg.sender].artistID,
             allArtists[msg.sender].songsPublished
-        );   
+        );
     }
-    
-    event songAdded (
-        uint songID,
+
+    event songAdded(
+        uint256 songID,
         string songName,
         string artistName,
-        uint price
+        uint256 price
     );
-    
-    function addSong(string memory _name, string memory _genre, string memory _hash, uint _price) public {
+
+    function addSong(
+        string memory _name,
+        string memory _genre,
+        string memory _hash,
+        uint256 _price
+    ) public {
         require(identifyUser[msg.sender] == UserType.ARTIST, "Not an artist.");
-        require(!songHashUsed[_hash], "Duplicate detected. Song hash already in use.");
-        
+        require(
+            !songHashUsed[_hash],
+            "Duplicate detected. Song hash already in use."
+        );
+
         songIDTracker += 1;
-        
+
         Song memory newSong;
         newSong.songID = songIDTracker;
         newSong.songName = _name;
@@ -113,44 +145,87 @@ contract blockstudio {
         allSongs[songIDTracker] = newSong;
         allArtists[msg.sender].songsPublished.push(songIDTracker);
         songHashUsed[_hash] = true;
-        
-        emit songAdded(newSong.songID, newSong.songName, newSong.artistName, newSong.price);
+
+        emit songAdded(
+            newSong.songID,
+            newSong.songName,
+            newSong.artistName,
+            newSong.price
+        );
     }
-    
-    event songPurchased (
-        uint songID,
+
+    event songPurchased(
+        uint256 songID,
         string songName,
         string audienceName,
-        uint price
+        uint256 price
     );
-    
-    function buySong(uint _songID) public payable {
-        require(identifyUser[msg.sender] == UserType.AUDIENCE, "Not an audience member.");
-        require(!allAudience[msg.sender].isSongPurchased[_songID], "Cannot buy the song twice.");
-        
+
+    function buySong(uint256 _songID) public payable {
+        require(
+            identifyUser[msg.sender] == UserType.AUDIENCE,
+            "Not an audience member."
+        );
+        require(
+            !allAudience[msg.sender].isSongPurchased[_songID],
+            "Cannot buy the song twice."
+        );
+
         Song memory curSong = allSongs[_songID];
-        
-        require(msg.value == (curSong.price * 1 wei), "Amount payed does not match price of the song.");
+
+        require(
+            msg.value == (curSong.price * 1 wei),
+            "Amount payed does not match price of the song."
+        );
         require(msg.sender.balance > msg.value, "Insufficient balance.");
 
         curSong.artistAddress.transfer(msg.value);
         timesSongPurchased[_songID] += 1;
-        
+
         allAudience[msg.sender].songsPurchased.push(_songID);
         allAudience[msg.sender].isSongPurchased[_songID] = true;
-        
-        emit songPurchased(curSong.songID, curSong.songName, allAudience[msg.sender].name, msg.value);
+
+        emit songPurchased(
+            curSong.songID,
+            curSong.songName,
+            allAudience[msg.sender].name,
+            msg.value
+        );
     }
-    
-    function getSongDetails(uint _songID) public view returns(string memory, string memory, string memory, string memory, uint, uint){
-        return (allSongs[_songID].songName, 
-            allSongs[_songID].artistName, 
-            allSongs[_songID].genre, 
+
+    event artistDonated(uint256 artistID, string audienceName, uint256 amount);
+
+    function donateArtist(uint256 _artistID) public payable {
+        require(
+            identifyUser[msg.sender] == UserType.AUDIENCE,
+            "Not an audience member."
+        );
+        require(msg.sender.balance > msg.value, "Insufficient balance.");
+
+        getArtistAddress[_artistID].transfer(msg.value);
+
+        emit artistDonated(_artistID, allAudience[msg.sender].name, msg.value);
+    }
+
+    function getSongDetails(uint256 _songID)
+        public
+        view
+        returns (
+            string memory,
+            string memory,
+            string memory,
+            string memory,
+            uint256,
+            uint256
+        )
+    {
+        return (
+            allSongs[_songID].songName,
+            allSongs[_songID].artistName,
+            allSongs[_songID].genre,
             allSongs[_songID].hash,
             allSongs[_songID].price,
             timesSongPurchased[_songID]
         );
     }
-
-    
 }
