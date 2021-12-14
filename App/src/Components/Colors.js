3aac19d5e5aa87267f417cc684fc95f89aba01f0
Anuneet Anand <anuneetanand@gmail.com>
2021-12-14 17:16:07 +0530

    Update Colors.js

--- App/src/Components/Colors.js
+++ App/src/Components/Colors.js
@@ -1,8 +1,9 @@
 export const COLORS = {
     white: 'rgb(240,240,230)',
     brown: '#ffdd95',
-    highlight: '#fdf255',
+    highlight: '#BCDAB4',
     black: 'rgb(4,16,13)',
+    blue :'#143642',
     blurWhite: 'rgb(255,255,255,0.75)',
     blurBlack: 'rgb(0,0,0,0.75)',
   }
